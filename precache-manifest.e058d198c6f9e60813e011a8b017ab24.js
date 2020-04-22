e718d5df3cdbb62efce1f8b1594a2bf0bbdaa0ac
robertom123 <netpig00@gmail.com>
2020-04-22 00:25:13 -0700

    Updates

---
+++ precache-manifest.e058d198c6f9e60813e011a8b017ab24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da8d3fbf9692d80679eb9513795cc81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9133d42e6753ece42234",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af5bc0b16dc1f4bd632",
+    "url": "/static/js/2.8243e86f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8243e86f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9133d42e6753ece42234",
+    "url": "/static/js/main.bdee4705.chunk.js"
+  },
+  {
+    "revision": "dc72f2cadaabb83f95c9",
+    "url": "/static/js/runtime-main.9241484c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
